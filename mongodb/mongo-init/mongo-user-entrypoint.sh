3c7cc1dcbe214d1853989454f82fe6f695013feb
instance-id <48368821+instance-id@users.noreply.github.com>
2019-04-09 22:14:44 -0500

    Update mongo-user-entrypoint.sh

--- mongodb/mongo-init/mongo-user-entrypoint.sh
+++ mongodb/mongo-init/mongo-user-entrypoint.sh
@@ -4,8 +4,6 @@ echo "------------------------------------------------------------"
 echo "Begin creating application user and db."
 echo "------------------------------------------------------------"
 
-echo "${NODEBB_MONGO_DB} : ${MONGO_ROOT_USER} : ${MONGO_ROOT_PASS} : ${NODEBB_MONGO_USER} : ${NODEBB_MONGO_PASS} : ${NODEBB_MONGO_DB}"
-
 mongo ${NODEBB_MONGO_DB} \
         --host localhost \
         --port 27017 \
